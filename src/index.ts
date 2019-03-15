c1a520701a980177e42fdea5c0011677bfe3dac0
Aneesh Kallarakkal <kallarakkal@hotmail.com>
2019-03-15 13:48:30 +0100

    Add advertiser campaigns endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,71 @@
 import express from 'express';
 import csv from 'csv-parser';
 import fs from 'fs';
+import request from 'request';
+const ip = '127.0.0.1';
 const app = express();
 const port = 3000;
+const api_url = 'http://' + ip + ':' + port;
 
 interface Advertiser {
     id: number,
     name: string
 }
-app.get('/advertisers', (req, res, next) =>{
-    const csv = require('csv-parser');
+interface Campaign {
+    id: number,
+    advertiser_id: number,
+    name: string,
+    cost_model: 'per_impression' | 'per_click'| 'per_install',
+    cost: string
+}
+interface AdvertisersCampaigns {
+    name: string,
+    cost: number,
+    number_of_campaigns: number
+}
+
+app.get('/advertisers', (req, res) =>{
     const results: Advertiser[] = [];
     fs.createReadStream(__dirname + '/../data/advertisers.csv')
     .pipe(csv())
     .on('data', function(data: Advertiser){
         results.push(data);
     })
-    .on('end',function(data: {}){
+    .on('end',function(){
         res.json(results);
     });  
+})
+
+app.get('/advertisers_campaigns', (req, res) => {
+    request({ url : api_url + '/advertisers', json: true}, (error, r, advertisers: Advertiser[]) =>  {
+        const campaigns: Campaign[] = [];
+        fs.createReadStream(__dirname + '/../data/campaigns.csv')
+        .pipe(csv())
+        .on('data', function(data: Campaign){
+            campaigns.push(data);
+        })
+        .on('end',function(){
+            const advertisementCampaigns = advertisers.map((advertiser) => {
+                const advertisementCampaign:AdvertisersCampaigns = {
+                    name : advertiser.name,
+                    cost: 0,
+                    number_of_campaigns: 0
+                };
+
+                campaigns
+                    .filter(campaign => campaign.advertiser_id === advertiser.id)
+                    .reduce((aC, campaign) => {
+                        aC.cost += parseFloat(campaign.cost);
+                        aC.number_of_campaigns++;
+                        
+                        return aC;
+                    }, advertisementCampaign)
+
+                return advertisementCampaign;
+            })
+            res.send(advertisementCampaigns);
+        });  
+    })
 })
 
 
